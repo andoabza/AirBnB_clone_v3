802c2f898934d8d756a88204e4e78a319d2a1e66
anda <terbu102@gmail.com>
2023-10-27 21:55:14 -0700

    strict

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,9 @@ from flask import jsonify
 from api.v1.views import app_views
 
 
+app_views.url_map.strict_slashes = False
+
+
 @app_views.route('/status', methods=['GET'])
 def status():
     """ a route to the statue"""
