edf7c27b13dc460a381b63769323847117463a70
anda <terbu102@gmail.com>
2023-10-28 00:33:52 -0700

    readme

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,28 @@
 """ a script to route json data """
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
+from models.state import State
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.user import User
+from models.review import Review
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ a route to the statue"""
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """a function that return all storage json"""
+    return jsonify(
+            {"amenities": storage.count(Amenity),
+                "cities": storage.count(City),
+                "places": storage.count(Place),
+                "reviews": storage.count(Review),
+                "states": storage.count(State),
+                "users": storage.count(User)})
