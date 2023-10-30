34de520c99ba1bc4367e4ee5a879c9ee65d01af8
anda <terbu102@gmail.com>
2023-10-30 10:17:03 -0700

    app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,32 +1,36 @@
 #!/usr/bin/python3
-""" main flask app route"""
-from os import getenv
-from flask import Flask, jsonify
+"""
+module to create Flask app that works with the API
+"""
 from models import storage
+from flask import Flask, jsonify
+from flask_cors import CORS
 from api.v1.views import app_views
-
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+app.config.update(JSONIFY_PRETTYPRINT_REGULAR=True)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
 def tear_down(self):
+    """
+    tears down
+    """
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(e):
-    """ 404 error handler"""
-    return jsonify({"error": "Not found"}), 404
+def not_found(error):
+    """
+    error handler for 404
+    """
+    return (jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
-    """ getenv for main app"""
-    host = getenv('HBNB_API_HOST')
-    port = getenv('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.getenv("HBNB_API_PORT", 5000))
     app.run(host=host, port=port, threaded=True)
